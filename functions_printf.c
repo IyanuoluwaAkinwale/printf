c46735b6aeaf89a3c655f7eb168113ee1b10036c
IyanuoluwaAkinwale <ashjaley@gmail.com>
2023-10-20 09:04:42 +0100

    handled some variables

--- functions_printf.c
+++ functions_printf.c
@@ -155,9 +155,9 @@ int print_int(va_list types, char buffer[],
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	unsigned int n, m, i, sum;
+	unsigned int p, q, j, sum;
 	unsigned int a[32];
-	int count;
+	int counter;
 
 	UNUSED(buffer);
 	UNUSED(flags);
@@ -165,24 +165,24 @@ int print_binary(va_list types, char buffer[],
 	UNUSED(precision);
 	UNUSED(size);
 
-	n = va_arg(types, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
+	p = va_arg(types, unsigned int);
+	q = 2147483648; /* (2 ^ 31) */
+	a[0] = p / q;
+	for (j = 1; j < 32; j++)
 	{
-		m /= 2;
-		a[i] = (n / m) % 2;
+		p /= 2;
+		a[j] = (p / q) % 2;
 	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	for (j = 0, sum = 0, counter = 0; j < 32; j++)
 	{
 		sum += a[i];
-		if (sum || i == 31)
+		if (sum || j == 31)
 		{
-			char z = '0' + a[i];
+			char y = '0' + a[j];
 
-			write(1, &z, 1);
-			count++;
+			write(1, &y, 1);
+			counter++;
 		}
 	}
-	return (count);
+	return (counter);
 }
